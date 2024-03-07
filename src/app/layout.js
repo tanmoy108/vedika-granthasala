9bebd3905eeb7f65421b7f89e929b2d41bb72cd8
ShTanmoy <tanmooysharma46@gmail.com>
2024-03-07 23:32:36 +0600

    opengraph protocol added with image

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,12 +4,16 @@ import CommonLayout from "./features/navbar/commonLayout";
 import Providers from "@/redux/provider";
 import Footer from "./component/footer";
 import Hidden from "./features/blogs/hidden";
+import logoImage from "../../public/onlylogo.png"
 
 const montserrat = Montserrat({ subsets: ["latin"], weight: "500",display:'swap' });
 
 export const metadata = {
   title: "Vedika Granthasala",
   description: "Vedika Granthasala, your one-stop online destination for a spiritual journey through the vast world of Hinduism. Immerse yourself in a treasure trove of knowledge as we offer an extensive collection of books, adorned with intricate poster designs featuring powerful mantras and shlokas. Explore our enriching blogs on Hinduism, generously shared without any cost.",
+  openGraph: {
+    images: `${logoImage}`
+  },
 };
 
 
